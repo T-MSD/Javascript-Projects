571f65550fb1315f1f2bbde8158f447a0e4dba38
T-MSD <dias.tiago03@gmail.com>
2024-09-27 16:47:03 +0100

    added fist hangman funcs

--- Hangamn/lib/hangman.rb
+++ Hangamn/lib/hangman.rb
@@ -1,1 +1,29 @@
 # frozen_string_literal: true
+
+# Class that handles hangman logic
+class Hangman
+  attr_reader :current
+
+  def initialize(word)
+    @word = word
+    @guessed_letters = []
+  end
+
+  def add_letter(letter)
+    @guessed_letters.push(letter) if letter?(letter)
+  end
+
+  def display_current(guessed_letters)
+    @current = @word.chars.map { |letter| guessed_letters.include?(letter) ? letter : '_' }.join(' ')
+  end
+
+  def winner?
+    !@current.include?('_')
+  end
+
+  private
+
+  def letter?(letter)
+    @word.include?(letter)
+  end
+end
